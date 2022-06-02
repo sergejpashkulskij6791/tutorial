28c5fea9cd43505cce81636934236d1a940c8ecc
Sergej <comfy4892@gmail.com>
2022-06-02 20:38:01 +0300

    create index/html

---
+++ javascript/js.js
@@ -0,0 +1,11 @@
+// todo Example 4 - Клас Math
+/*
+ * Напиши скрипт, який виводить в консоль заокруглені вгору / вниз і т.д. значення
+ змінної value. Використовуй методи Math.floor(), Math.ceil() і Math.round(). Перевір що буде в консолі при значеннях 27.3 та 27.9.
+ */
+console.log(Math.pow(2, 8));
+const value = 27.3;
+
+console.log(Math.floor(value));
+console.log(Math.ceil(value));
+console.log(Math.round(value));
